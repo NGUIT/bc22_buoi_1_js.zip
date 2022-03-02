1d18233ca8a395ee618a404c69c6b7a7343a35b9
NGUIT <cntt3121@gmail.com>
2022-03-02 22:31:58 +0700

    loag jabuoi1

---
+++ java.js
@@ -0,0 +1,91 @@
+
+//bài 1
+var ngay;
+
+ngay= 1;
+
+var luong = 100;
+
+
+var tong = ngay * luong;
+
+console.log("Tiền lương ngày:" + tong);
+
+//bài 2
+
+var nummber1;
+var nummber2;
+var nummber3;
+var nummber4;
+var nummber5;
+
+nummber1 =25;
+nummber2 =5;
+nummber3 =10;
+nummber4 =15;
+nummber5 =20;
+
+var tb=(  nummber1+nummber2+nummber3+nummber4+nummber5) /5 ;
+
+console.log( "Trung bình là"+tb);
+
+//bài 3
+
+
+var USD;
+USD=2;
+var vnd=23.500;
+
+var X=USD*vnd;
+
+console.log("Số tiền quy đồi là: "+X + ".000 VND");
+
+//bài 4
+
+var cd;
+
+var cr;
+
+dc=2;
+cr=4;
+
+var dt=dc*cr;
+
+var cv=(dc+cr)*2;
+
+console.log("Chiều dài là: " + dt + "chu vi là :" + cv);
+
+//bài 5:
+
+var n=12;
+var m=44;
+var a=83;
+var donvi1= Math.floor(n % 10);
+
+var hangchuc1=Math.floor(n / 10);
+
+
+var kq1=donvi1 + hangchuc1;
+
+console.log ("12=> 1+2 =" + kq1);
+
+var donvi2= Math.floor(m % 10);
+
+var hangchuc2=Math.floor(m / 10);
+
+var kq2=donvi2 + hangchuc2;
+
+console.log ("44=> 4+4 =" + kq2);
+
+var donvi3= Math.floor(a % 10);
+
+var hangchuc3=Math.floor(a / 10);
+
+var kq3= donvi3 + hangchuc3;
+
+console.log ("83=> 8+3 =" + kq3);
+
+
+
+
+
